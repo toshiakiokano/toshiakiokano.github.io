a2875779c0b41871c30ec895b8c738302770db09
toshiaki okano <toshiakiokano1982@gmail.com>
2018-11-05 14:21:28 +0900

    fixed

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+'use strict'
+
 const medias = {audio: false, video; {
-    facingMode: {
-        exact: 'environment'
-    }
-}};
-const video = document.getElementById("video");
+                    facingMode: {
+                        exact: 'environment'
+                    }
+                }},
+    video = document.getElementById("video"),
+    canvas = document.getElementById("canvas"),
+    ctx = canvas.getContext("2d");
+
+let imgData, data, ave;
 
 navigator.getUserMedia(medias, successCallback, errorCallback);
+
+
+requestAnimationFrame(draw);
 
 function successCallback(stream) {
     video.srcObject = stream;
@@ -13,3 +22,26 @@ function successCallback(stream) {
 function errorCallback(error) {
     alert(error);
 }
+
+function draw() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    ctx.drawImage(video, 0, 0);
+
+    imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    data = imgData.data;
+
+    for(let i = 0; i < data.length; i += 4) {
+        ave = (data[i + 0] + data[i + 1] + data[i + 2]) / 3;
+
+        data[i + 0] =
+            data[i + 1] =
+                data[i + 2] = (ave > 255 / 2) ? 255 : (ave > 255 / 4) ? 127 : 0;
+        data[i + 3] = 255;
+    }
+
+    ctx.putImageData(imgData, 0, 0);
+    requestAnimationFrame(draw);
+}
+
